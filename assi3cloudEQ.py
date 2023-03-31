ca59a11bcb8362d0ecd5e8e736f08857157b914c
AnnuDhull <102133265+AnnuDhull@users.noreply.github.com>
2023-03-31 10:52:17 +0530

    Add files via upload

---
+++ assi3cloudEQ.py
@@ -0,0 +1,41 @@
+# Difference between == and === :-
+# == operator checks whether the value is equal or not .
+# === compares the both values as well as data types but it is not used in python  .
+
+# pass statement instructs to do nothing 
+# a = 20
+# b = 80
+# if a<b :
+#     print("B is Greater")
+# elif a==b :
+#     print("A and B Equal")
+#     pass
+# else:
+#     print("A is Greater")
+# ---------------------------------------------------------------------------------------------------------------------------
+
+
+#  The break keyword is used to break from a whole loop.
+# i = 1
+# while i < 14:
+#   print(i)
+#   if i == 7:
+#     break
+#   i += 1
+
+#   The continue keyword is used to end the current iteration in a loop, and continues to the next iteration.
+
+# i = 0
+# while i < 14:
+#   i += 1
+#   if i == 8:
+#     continue
+#   print(i)
+
+# ------------------------------------------------------------------------------------------------------------------
+
+#   range (start , stop , step)
+
+# A = range(7,45,2)
+# for i in A:
+#   print(i)
